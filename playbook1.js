b822771cda1d72b3c33f8967c41973d0e99a9b5a
Peter7450 <ayomidepeter96@gmail.com>
2026-02-10 11:41:24 -0800

    Completed exercise 1-3

--- playbook1.js
+++ playbook1.js
@@ -25,3 +25,32 @@ function isAdult(age){
 }
 console.log(isAdult(18));
 console.log(isAdult(13));
+
+// Exercise 4: Create a function getFullName that takes first name and last name, returns full name.
+
+function getFullName(firstName, lastName){
+    return `${firstName} ${lastName}`;
+}
+console.log(getFullName("Declan", "Mostaf"));
+console.log(getFullName("Olive", "Vera"));
+
+// Exercise 5: Create a function calculateRectangleArea that takes width and height, returns area.
+
+function calculateRectangleArea(width, height){
+    area = width * height;
+    return area;
+}
+console.log(calculateRectangleArea(5, 5));
+console.log(calculateRectangleArea(10, 10));
+
+// Exercise 6: Create a function isEven that returns true if number is even, false if odd.
+
+function isEven(number){
+    if(number % 2 === 0){
+        return "true";
+    } else {
+        return "false";
+    }
+}
+console.log(isEven(4));
+console.log(isEven(9));
