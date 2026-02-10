c2addbf179b5d171e373370a1fe5e9226fa3d9b4
Peter7450 <ayomidepeter96@gmail.com>
2026-02-10 11:26:23 -0800

    Completed exercise 3-6

---
+++ playbook2.js
@@ -0,0 +1,22 @@
+// Exercise 4: Create a function getFullName that takes first name and last name, returns full name.
+
+function getFullName(firstName, lastName){
+    return `${firstName} ${lastName}`;
+}
+console.log(getFullName("Declan", "Mostaf"));
+console.log(getFullName("Olive", "Vera"));
+
+// Exercise 5: Create a function calculateRectangleArea that takes width and height, returns area.
+
+function calculateRectangleArea(width, height){
+    area = width * height;
+    return area;
+}
+console.log(calculateRectangleArea(5, 5));
+console.log(calculateRectangleArea(10, 10));
+
+// Exercise 6: Create a function isEven that returns true if number is even, false if odd.
+
+function isEven(){
+    
+}
