c9ffde064e8636c1048628b500725790ec1e1bb6
Peter7450 <ayomidepeter96@gmail.com>
2026-02-12 08:30:22 -0800

    Completed exercise 12-14

--- playbook1.js
+++ playbook1.js
@@ -115,3 +115,32 @@ function reverseString(name){
     return name.split("").reverse().join("");
 }
 console.log(reverseString("ayomide"));
+
+// Exercise 12: Create a function isValidPassword that returns true if password is 8+ characters.
+
+function isValidPassword(password){
+    if(password.length > 8){
+        return "true";
+    } else {
+        return "false";
+    }
+}
+console.log(isValidPassword("pass123"));
+console.log(isValidPassword("password123"));
+
+// Exercise 13: Create a function countItems that returns how many items are in an array.
+
+function countItems(numbers){
+    return numbers;
+}
+console.log(countItems([1, 2, 3]).length);
+console.log(countItems([1, 2, 3, 4, 5, 6, 7, 8, 9]).length);
+
+// Exercise 14: Create a function getLastItem that returns the last item in an array.
+
+function getLastItem(numbers){
+    lastItem = numbers[numbers.length - 1]
+    return lastItem;
+}
+console.log(getLastItem([1, 2, 3, 4, 5, 6, 7, 8, 9]));
+console.log(getLastItem([1, 2, 3, 4, 5, 6]));
--- playbook2.js
+++ playbook2.js
@@ -1,31 +1,4 @@
-// Exercise 10: Create a function isValidPassword that returns true if password is 8+ characters.
 
-function isValidPassword(password){
-    if(password.length > 8){
-        return "true";
-    } else {
-        return "false";
-    }
-}
-console.log(isValidPassword("pass123"));
-console.log(isValidPassword("password123"));
-
-// Exercise 11: Create a function countItems that returns how many items are in an array.
-
-function countItems(numbers){
-    return numbers;
-}
-console.log(countItems([1, 2, 3]).length);
-console.log(countItems([1, 2, 3, 4, 5, 6, 7, 8, 9]).length);
-
-// Exercise 12: Create a function getLastItem that returns the last item in an array.
-
-function getLastItem(numbers){
-    lastItem = numbers[numbers.length - 1]
-    return lastItem;
-}
-console.log(getLastItem([1, 2, 3, 4, 5, 6, 7, 8, 9]));
-console.log(getLastItem([1, 2, 3, 4, 5, 6]));
 
 // Exercise 14: Create a function findMax that returns the largest number in an array.
 
